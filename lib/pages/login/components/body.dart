d17cf3d2eb21ca3a8d3763b3851dd5d7d52c8e55
Wendell Arnald <wendell.arnald@gmail.com>
2020-11-03 23:06:08 +0000

    Add background class and responsibility separate.

--- lib/pages/login/components/body.dart
+++ lib/pages/login/components/body.dart
@@ -1,32 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/pages/login/components/background.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/main_top.png",
-              width: size.width * 0.3,
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/main_bottom.png",
-              width: size.width * 0.2,
-            ),
-          ),
-        ],
+    return Background(
+      child: Column(
+        children: <Widget>[],
       ),
     );
   }
