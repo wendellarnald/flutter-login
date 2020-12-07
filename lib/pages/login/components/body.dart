a88461231f4eb1909aaf94405616b99ea71ad64e
Wendell Arnald <wendell.arnald@gmail.com>
2020-12-07 22:02:49 +0000

    Add class components button

--- lib/pages/login/components/body.dart
+++ lib/pages/login/components/body.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/components/rounded_button.dart';
+import 'package:flutter_login/constants.dart';
 import 'package:flutter_login/pages/login/components/background.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
-import '../../../constants.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -18,49 +18,24 @@ class Body extends StatelessWidget {
             "WELCOME TO DEV",
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
+          SizedBox(
+            height: size.height * 0.02,
+          ),
           SvgPicture.asset(
             "assets/icons/chat.svg",
             height: size.height * 0.45,
           ),
-          RoudedButton(),
+          RoudedButton(
+            text: "LOGIN",
+            press: () {},
+          ),
+          RoudedButton(
+            text: "LOGIN",
+            color: primaryLighColor,
+            textColor: Colors.black,
+            press: () {},
+          ),
         ],
-      ),
-    );
-  }
-}
-
-class RoudedButton extends StatelessWidget {
-  final String text;
-  final Function press;
-  final Color color, textColor;
-
-  const RoudedButton({
-    Key key,
-    this.text,
-    this.press,
-    this.color,
-    this.textColor,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      width: size.width * 0.8,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(29),
-        child: FlatButton(
-          padding: EdgeInsets.symmetric(
-            vertical: 20,
-            horizontal: 40,
-          ),
-          color: primaryColor,
-          onPressed: () {},
-          child: Text(
-            "LOGIN",
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
       ),
     );
   }
