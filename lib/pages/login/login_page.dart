b1b8900f7f80989fab123167350ad0801da922aa
Wendell Arnald <wendell.arnald@gmail.com>
2020-12-07 22:18:29 +0000

    Include new folder for login page and login page call

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_login/pages/login/components/body.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Body(),
-    );
+    return Scaffold();
   }
 }
