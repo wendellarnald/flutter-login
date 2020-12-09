cb25077232e197b8d83f2fd5ece71fcd2e88224e
Wendell Arnald <wendell.arnald@gmail.com>
2020-12-09 23:00:53 +0000

    Add bottom image in login page

--- lib/pages/login/login.page.dart
+++ lib/pages/login/login.page.dart
@@ -12,11 +12,21 @@ class LoginPage extends StatelessWidget {
         child: Stack(
           children: <Widget>[
             Positioned(
+              top: 0,
+              left: 0,
               child: Image.asset(
                 "assets/images/main_top.png",
                 width: size.width * 0.3,
               ),
-            )
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Image.asset(
+                "assets/images/login_bottom.png",
+                width: size.width * 0.4,
+              ),
+            ),
           ],
         ),
       ),
