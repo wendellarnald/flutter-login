26b598ae574fe7d3fc4330c1664449d8116f0d7e
Wendell Arnald <wendell.arnald@gmail.com>
2020-12-10 23:27:59 +0000

    Separate body from background.

--- lib/pages/login/components/body.components.dart
+++ lib/pages/login/components/body.components.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/pages/login/components/background.components.dart';
 
 class Body extends StatelessWidget {
   const Body({
@@ -8,43 +9,9 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Background(size: size);
-  }
-}
-
-class Background extends StatelessWidget {
-  const Background({
-    Key key,
-    @required this.size,
-  }) : super(key: key);
-
-  final Size size;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      width: double.infinity,
-      height: size.height,
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/main_top.png",
-              width: size.width * 0.3,
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            child: Image.asset(
-              "assets/images/login_bottom.png",
-              width: size.width * 0.4,
-            ),
-          ),
-        ],
+    return Background(
+      child: Column(
+        children: <Widget>[],
       ),
     );
   }
