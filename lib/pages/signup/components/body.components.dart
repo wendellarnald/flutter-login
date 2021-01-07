46211e1d8c6bde06be133613715ba8b31cedd405
Wendell Arnald <wendell.arnald@gmail.com>
2021-01-07 21:13:36 +0000

    Separating body class layout

--- lib/pages/signup/components/body.components.dart
+++ lib/pages/signup/components/body.components.dart
@@ -10,6 +10,21 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Background(
+      child: Column(),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  final Widget child;
+  const Background({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
       height: size.height,
